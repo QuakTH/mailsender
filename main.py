657638f8c501c70ccd2efc3dd5718ad936e8b45b
Quack <bcde59@gmail.com>
2022-01-12 23:52:49 +0900

    size 조정 및 menubar 추가

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from tkinter import Tk
+from tkinter import Tk, filedialog, Menu
 
 
 def main():
@@ -6,6 +6,17 @@ def main():
     main 함수
     '''
     main_window = Tk()
+
+    main_window.title('메일 자동화 프로그램')
+    main_window.geometry('500x400')
+
+    menubar = Menu(main_window)
+
+    menu1 = Menu(menubar, tearoff=0)
+    menu1.add_command(label='열기')
+    menubar.add_cascade(label='파일', menu=menu1)
+
+    main_window.config(menu=menubar)
     main_window.mainloop()
 
 
