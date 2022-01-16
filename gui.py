26651192d52f37e3b4b87f26b85d625df169e787
Quack <bcde59@gmail.com>
2022-01-16 19:23:37 +0900

    dropdown 메뉴 및 파일 설명 추가

--- gui.py
+++ gui.py
@@ -1,4 +1,4 @@
-from tkinter import Label, Tk, filedialog, Button, Frame
+from tkinter import Label, OptionMenu, Tk, filedialog, Button, Frame,StringVar
 from tkinter.font import Font
 
 
@@ -34,20 +34,41 @@ class GUI():
         '''
         에셋들로 채우기
         '''
-        font1 = Font(family='굴림', size=15)
-        self.label1 = Label(
-            self.main_window, text='메일 자동 처리 프로그램 입니다.', pady=8, font=font1)
-        self.label1.pack()
-
-        self.frame1 = Frame(self.main_window,height=3)
+        self.frame1=Frame(self.main_window,height=3)
         self.frame1.pack()
 
-        self.button1 = Button(self.frame1, text='이메일 목록 파일',
+        font1 = Font(family='굴림', size=15)
+        self.label1 = Label(
+            self.frame1, text='메일 자동 처리 프로그램 입니다.', font=font1)
+        self.label1.pack(side='left')
+
+        email_site_list=('네이버','구글','다음')
+        self.var1=StringVar(self.frame1)
+        self.var1.set('사이트 선택')
+
+        self.dropdown1=OptionMenu(self.frame1,self.var1,*email_site_list)
+        self.dropdown1.config(font=font1)
+        self.dropdown1.pack(side='left')
+
+        self.frame2 = Frame(self.main_window,height=3)
+        self.frame2.pack()
+
+        self.button1 = Button(self.frame2, text='이메일 목록 파일',
                               font=font1, width=20, height=2, command=lambda :self.open_filedialog([('Excel 파일', '*.xlsx')]))
         self.button1.pack(side='left')
-        self.button2 = Button(self.frame1, text='보낼 email 내용',
+        self.button2 = Button(self.frame2, text='보낼 email 내용',
                               font=font1, width=15, height=2, command=lambda :self.open_filedialog([('텍트스 파일', '*.txt')]))
         self.button2.pack(side='left')
+
+        self.frame3=Frame(self.main_window,height=3)
+        self.frame3.pack()
+        self.label3 = Label(
+            self.frame3, text='이메일 목록 파일명 :', pady=8, font=font1)
+        self.label3.pack()
+
+        self.label4 = Label(
+            self.frame3, text='<보내지는 내용 예시>', pady=8, font=font1)
+        self.label4.pack()
     def open_filedialog(self, file_types):
         '''
         버튼을 눌렀을 때, filedialog를 띄우고
