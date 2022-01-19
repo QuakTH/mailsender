f5e9c4a3a205c1a9e48e8c948038c9e79dde745a
Quack <bcde59@gmail.com>
2022-01-19 23:26:54 +0900

    document 추가

--- main_window.py
+++ main_window.py
@@ -2,17 +2,29 @@ from PyQt5.QtWidgets import QWidget, QDesktopWidget
 
 
 class MainWindow(QWidget):
+    '''
+    메인 UI
+    '''
     def __init__(self):
+        '''
+        생성자
+        '''
         super().__init__()
         self.ui_init()
 
     def ui_init(self):
+        '''
+        UI를 생성하는 실질적인 메소드
+        '''
         self.setWindowTitle('메일 자동화 프로그램')
         self.resize(600, 400)
         self.center_window()
         self.show()
 
     def center_window(self):
+        '''
+        window를 화면 중앙에 배치
+        '''
         frame_geom=self.frameGeometry()
         center=QDesktopWidget().availableGeometry().center()
         frame_geom.moveCenter(center)
