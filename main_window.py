df883c8620cf69237ad79557424f139f3352a4c2
Quack <bcde59@gmail.com>
2022-01-19 23:59:10 +0900

    메일 보낼 사이트 선택하는 UI 구현

--- main_window.py
+++ main_window.py
@@ -1,10 +1,13 @@
-from PyQt5.QtWidgets import QWidget, QDesktopWidget
+from PyQt5.QtWidgets import QWidget, QDesktopWidget, QLabel, QVBoxLayout, QHBoxLayout, QRadioButton, QButtonGroup
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QFont
 
 
 class MainWindow(QWidget):
     '''
     메인 UI
     '''
+
     def __init__(self):
         '''
         생성자
@@ -18,6 +21,39 @@ class MainWindow(QWidget):
         '''
         self.setWindowTitle('메일 자동화 프로그램')
         self.resize(600, 400)
+
+        # 메일 보낼 수단 선택 layout
+        mail_select_hbox = QHBoxLayout()
+        mail_select_vbox = QVBoxLayout()
+
+        mail_select_label = QLabel('메일 사이트 선택 :')
+        mail_select_label.setAlignment(Qt.AlignmentFlag.AlignVCenter)
+        mail_select_label.setFont(QFont('굴림', 13, 90))
+
+        radio_group=QButtonGroup()
+
+        naver_radio_btn = QRadioButton('네이버')
+        naver_radio_btn.setFont(QFont('굴림', 13))
+        radio_group.addButton(naver_radio_btn)
+
+        google_radio_btn = QRadioButton('구글')
+        google_radio_btn.setFont(QFont('굴림', 13))
+        radio_group.addButton(google_radio_btn)
+
+        daum_radio_btn = QRadioButton('다음')
+        daum_radio_btn.setFont(QFont('굴림', 13))
+        radio_group.addButton(daum_radio_btn)
+
+        mail_select_hbox.addStretch(1)
+        mail_select_hbox.addWidget(mail_select_label)
+        mail_select_hbox.addStretch(1)
+        mail_select_hbox.addWidget(naver_radio_btn)
+        mail_select_hbox.addWidget(google_radio_btn)
+        mail_select_hbox.addWidget(daum_radio_btn)
+        mail_select_hbox.addStretch(1)
+
+        self.setLayout(mail_select_hbox)
+
         self.center_window()
         self.show()
 
@@ -25,7 +61,7 @@ class MainWindow(QWidget):
         '''
         window를 화면 중앙에 배치
         '''
-        frame_geom=self.frameGeometry()
-        center=QDesktopWidget().availableGeometry().center()
+        frame_geom = self.frameGeometry()
+        center = QDesktopWidget().availableGeometry().center()
         frame_geom.moveCenter(center)
         self.move(frame_geom.topLeft())
