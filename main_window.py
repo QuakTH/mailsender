39a461e8ecfd05b42dec8a9b95445d8de046a16b
Quack <bcde59@gmail.com>
2022-01-23 23:48:44 +0900

    메일 사이트 선택 레이아웃 구성 따로 분리

--- main_window.py
+++ main_window.py
@@ -13,6 +13,7 @@ class MainWindow(QWidget):
         생성자
         '''
         super().__init__()
+        self.vert_layout = QVBoxLayout()
         self.ui_init()
 
     def ui_init(self):
@@ -23,14 +24,36 @@ class MainWindow(QWidget):
         self.resize(600, 400)
 
         # 메일 보낼 수단 선택 layout
+        mail_select_layout = self.create_mail_select()
+
+        self.vert_layout.addStretch(1)
+        self.vert_layout.addLayout(mail_select_layout)
+        self.vert_layout.addStretch(3)
+        self.setLayout(self.vert_layout)
+
+        self.center_window()
+        self.show()
+
+    def center_window(self):
+        '''
+        window를 화면 중앙에 배치
+        '''
+        frame_geom = self.frameGeometry()
+        center = QDesktopWidget().availableGeometry().center()
+        frame_geom.moveCenter(center)
+        self.move(frame_geom.topLeft())
+
+    def create_mail_select(self):
+        '''
+        메일 보내는 사이트를 정하는 layout을 구현하는 메소드
+        '''
         mail_select_hbox = QHBoxLayout()
-        mail_select_vbox = QVBoxLayout()
 
         mail_select_label = QLabel('메일 사이트 선택 :')
         mail_select_label.setAlignment(Qt.AlignmentFlag.AlignVCenter)
         mail_select_label.setFont(QFont('굴림', 13, 90))
 
-        radio_group=QButtonGroup()
+        radio_group = QButtonGroup()
 
         naver_radio_btn = QRadioButton('네이버')
         naver_radio_btn.setFont(QFont('굴림', 13))
@@ -52,16 +75,4 @@ class MainWindow(QWidget):
         mail_select_hbox.addWidget(daum_radio_btn)
         mail_select_hbox.addStretch(1)
 
-        self.setLayout(mail_select_hbox)
-
-        self.center_window()
-        self.show()
-
-    def center_window(self):
-        '''
-        window를 화면 중앙에 배치
-        '''
-        frame_geom = self.frameGeometry()
-        center = QDesktopWidget().availableGeometry().center()
-        frame_geom.moveCenter(center)
-        self.move(frame_geom.topLeft())
+        return mail_select_hbox
