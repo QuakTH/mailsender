6977a5aaf53ab3e50fb138d6dbc3916728b37c50
Quack <bcde59@gmail.com>
2024-04-28 00:00:20 +0900

    exception이 있는 경우의 메소드 실행 wrapper function

---
+++ utils.py
@@ -0,0 +1,17 @@
+from exceptions import CustomException
+
+
+def run_log_exception(log_output_name: str):
+    def run_callable(callable):
+        def wrapper(self, *args, **kwargs):
+            log_output = getattr(self, log_output_name)
+            try:
+                callable(self, *args, **kwargs)
+            except CustomException as e:
+                log_output.append(e.msg)
+            except Exception:
+                log_output.append("Something Went Wrong.. Try to restart the app.")
+
+        return wrapper
+
+    return run_callable
